18f1d76f5ca655f834e6b5ecf2ac33f9365fa7ff
ValeryVVV <110850242+ValeryVVV@users.noreply.github.com>
2023-03-15 21:07:42 +0200

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,14 @@
-import React, { Component } from "react";
+import { useState } from "react";
 import { FeedbackOptions } from "./Feedback/FeedbackOptions/FeedbackOptions";
 import { Notification } from "./Feedback/Notification/Notification";
 import { Section } from "./Feedback/Section/Section";
 import { Statistics } from "./Feedback/Statistics/Statistics";
 import style from './Feedback/Feedback.module.css';
-import { useState } from "react";
+
 
 
 function App() {
-    
+
     const [good, setGood] = useState(0);
     const [neutral, setNeutral] = useState(0);
     const [bad, setBad] = useState(0);
